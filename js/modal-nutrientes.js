9d8bb8184805dfbf7ce4ff67993e8802f2db56a9
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-29 11:23:49 -0300

    Ajustes

--- js/modal-nutrientes.js
+++ js/modal-nutrientes.js
@@ -3,9 +3,13 @@ const modalProteina = document.querySelector(".js-nutri-modal-proteina");
 
 btnProteina.addEventListener("click", () => {
     modalProteina.classList.add("ativo");
+    const menu = document.querySelector(".js-menu-mobile");
+    menu.classList.add("desativar");
 });
 
 const bntFecharPro = document.querySelector(".nutri__ico--fecharPro");
     bntFecharPro.addEventListener("click", () => {
         modalProteina.classList.remove("ativo");
+        const menu = document.querySelector(".js-menu-mobile");
+        menu.classList.remove("desativar");
     });
