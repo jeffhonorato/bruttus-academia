740a1f7189d3b73542512b61f3f5f087041df8e9
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-09-29 12:49:47 -0300

    Ajustes

--- js/modal-nutrientes.js
+++ js/modal-nutrientes.js
@@ -1,19 +1,23 @@
-const btnProteina = document.querySelector(".js-btn-proteina");
-const modalProteina = document.querySelector(".js-nutri-modal-proteina");
-
-btnProteina.addEventListener("click", () => {
-    modalProteina.classList.add("ativo");
-    const menu1 = document.querySelector(".js-menu-mobile");
-    menu1.classList.add("desativar");
-    const scroll = document.querySelector(".js-scroll");;
-    scroll.classList.add("off");
-});
-
-const bntFecharPro = document.querySelector(".nutri__ico--fecharPro");
-    bntFecharPro.addEventListener("click", () => {
-        modalProteina.classList.remove("ativo");
+const proteina = () => {
+    const btnProteina = document.querySelector(".js-btn-proteina");
+    const modalProteina = document.querySelector(".js-nutri-modal-proteina");
+    
+    btnProteina.addEventListener("click", () => {
+        modalProteina.classList.add("ativo");
         const menu1 = document.querySelector(".js-menu-mobile");
-        menu1.classList.remove("desativar");
-        const scroll = document.querySelector(".js-scroll")
-    scroll.classList.remove("off");
+        menu1.classList.add("desativar");
+        const scroll = document.querySelector(".js-scroll");;
+        scroll.classList.add("off");
     });
+    
+    const bntFecharPro = document.querySelector(".nutri__ico--fecharPro");
+        bntFecharPro.addEventListener("click", () => {
+            modalProteina.classList.remove("ativo");
+            const menu1 = document.querySelector(".js-menu-mobile");
+            menu1.classList.remove("desativar");
+            const scroll = document.querySelector(".js-scroll")
+        scroll.classList.remove("off");
+    });
+}
+
+proteina();
